da927130b4e7aafa9c84b2d153e21c3111baea74
Abdur-Rahman Olaniyan <165938727+AbdurRahman-Olaniyan@users.noreply.github.com>
2025-10-23 12:17:58 +0100

    Clarify ETL process in proc_load_silver.sql
    Updated comments to clarify the source of data and actions performed.

--- Script/Silver/proc_load_silver.sql
+++ Script/Silver/proc_load_silver.sql
@@ -5,10 +5,10 @@ Stored Procedure: Load Silver Layer (Bronze -> Silver)
 ===============================================================================
 Script Purpose:
     This stored procedure performs the ETL (Extract, Transform, Load) process to 
-    populate the 'silver' schema tables from the 'bronze' schema.
+    populate the 'silver' schema table from the datawarehouse 'bronze' schema.
 	Actions Performed:
 		- Truncates Silver tables.
-		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver layer.
 		
 Parameters:
     None. 
